0a8491373a32f14131446719d2d537939f529857
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-08 21:51:15 -0300

    :sparkles: feat: Configuração basica

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +10,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className} antialiased container min-h-screen flex flex-col items-center justify-center px-4 py-2`}
       >
         {children}
       </body>
