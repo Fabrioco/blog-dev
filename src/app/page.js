af9f51d842e8ff72da90fffa1911ffd8a47a22ba
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-09 00:15:45 -0300

    :sparkles: feat: Hook para verificar se ta logado

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
-'use client'
-
+"use client";
+import { useAuthentication } from "@/hooks/useAuthentication";
 import { useRouter } from "next/navigation";
 
-export default function IndexPage(){
+export default function IndexPage() {
   const router = useRouter();
+  const { user, isLoading } = useAuthentication();
 
-  router.push('/login')
+  if (isLoading) {
+    return (
+      <main className="w-full h-full flex flex-col items-center justify-center">
+        <h1>Carregando...</h1>
+      </main>
+    );
+  }
+
+  const logo = "</>";
+  return (
+    <main className="w-full h-full flex flex-col items-center justify-center">
+      <h1>Blog Dev {logo}</h1>
+      {user ? <p>Bem-vindo, {user.name}!</p> : <p>Usuário não autenticado</p>}
+    </main>
+  );
 }
